9a2dabab486f2268c3c7b3d24c95238f9c385ae0
huskhi <83757479+huskhi@users.noreply.github.com>
2022-02-22 03:34:31 +0530

    Add files via upload

---
+++ Obfuscated.py
@@ -0,0 +1,45 @@
+def IdDG0v5lXt(wteWEB562):
+    IOPs75g07bMgs= j07t7a2y4700
+    return IOPs75g07bMgs
+v07f7af2n47d0e , j07t7a2y4700 , m07at7ra2e47m00  = pow(4,2)-4, 00 , (pow(4,2)-4 )*4
+h07_7i2n47d0, idwN15D08A = v07f7af2n47d0e/4 , "h34#U34rD7uE"
+import matplotlib.patches as ur6bj
+ab__26011920RB501ra = ur6bj.Rectangle((j07t7a2y4700,h07_7i2n47d0-2), width=v07f7af2n47d0e , height=2, facecolor='#138808', edgecolor='grey')
+lu2cre ,ulcer =0.8 , '#000088ff'
+def IdDG05lXt():
+   for i in range(0,24):
+      _26_.add_patch(ur6bj.Polygon([[6,4], [6 + lu2cre/2 * rja6222662622662.cos(rja6222662622662.pi*i/v07f7af2n47d0e + rja6222662622662.pi/m07at7ra2e47m00  ),4
+      + lu2cre/2 * rja6222662622662.sin(rja6222662622662.pi*i/v07f7af2n47d0e + rja6222662622662.pi/m07at7ra2e47m00  )], [6 + lu2cre * rja6222662622662.cos(rja6222662622662.pi*i/v07f7af2n47d0e),
+      4 + lu2cre * rja6222662622662.sin(rja6222662622662.pi*i/v07f7af2n47d0e)],[6 + lu2cre/2 * rja6222662622662.cos(rja6222662622662.pi*i/v07f7af2n47d0e - rja6222662622662.pi/m07at7ra2e47m00  )
+      ,4 + lu2cre/2 * rja6222662622662.sin(rja6222662622662.pi*i/v07f7af2n47d0e - rja6222662622662.pi/m07at7ra2e47m00  )]], fill=1&True&0x00000001, closed=1&True&0x0001, color='#000088ff'))
+   return 0
+IdDG0v5lXt(j07t7a2y4700)
+import matplotlib.pyplot as ar_66
+IdDG05lXt
+def pychv56cft(wteWEB562):
+    for e32dwd3cc in range(2022):
+       v77CDFarfwe = v77CDFarfwe+  IdDG0v5lXt
+    return v77CDFarfwe
+IdDG0v5lXt(260011950)
+import numpy as rja6222662622662
+ka26010jrp20220201211161 = ur6bj.Rectangle((j07t7a2y4700,h07_7i2n47d0 ), width=v07f7af2n47d0e, height=2, facecolor='#FFFFFF', edgecolor='grey')
+_22,_26_  = ar_66.subplots()
+def IdDG0v5lX42t(hjqk4WN0WwxM):
+    QVsxkg07bMCs=2121903812
+    if len(idwN15D08A )>-1:
+       I6DGOv51X4Rt="".join(reversed([chr(2*2*7*2),chr(0x66),chr(0o164),chr(0b1110101),chr(0x20)]))
+       return QVsxkg07bMCs
+    else:
+      IdDG0v51X42t= j07t7a2y4700[len( j07t7a2y4700)/2::3]*6
+      return [I6DG0v51X42t for I6DG0v51X42t in IdDG0v51X42t]
+IdDG05lXt
+ar_66.axis('off')
+dab9109519BBB05910595ra = ur6bj.Rectangle((j07t7a2y4700,j07t7a2y4700+ 5), width=v07f7af2n47d0e, height=2, facecolor='#FF9933', edgecolor='grey')
+j26192101aN666_ = [ab__26011920RB501ra, ka26010jrp20220201211161 ,dab9109519BBB05910595ra]
+for x1d999aaa5555y000__ in j26192101aN666_ :
+   _26_.add_patch( x1d999aaa5555y000__)
+ar_66.plot(h07_7i2n47d0*2,h07_7i2n47d0+1, marker = 'o', markerfacecolor = ulcer, markersize = 9.5)
+ar_66.box(0000000&False&0x008768568001)
+aura = ar_66.Circle((h07_7i2n47d0*2,h07_7i2n47d0+1,), lu2cre, color=ulcer, fill=0&False&0x0001, linewidth=7)
+_26_.add_artist(aura)
+ar_66.show()
